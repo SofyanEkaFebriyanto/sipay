d6e8651f3d3f0384e318de7914a234553cd0aef2
SofyanEkaFebriyanto <sofyanekafebriyanto@gmail.com>
2026-05-10 01:05:28 +0700

    setting tailwind + implementasi ui login

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+Route::get('/', [AuthController::class, 'index']);
+Route::get('/login', [AuthController::class, 'index'])->name('login');
